948f4dcc4cf496c9e91801e8bb419b2a81253427
Yeongho Kim <yeonghoey@gmail.com>
2020-06-10 04:53:45 +0900

    Refactor server/anki_test.go

--- backend/server/anki_test.go
+++ backend/server/anki_test.go
@@ -11,43 +11,35 @@ func TestAnki(t *testing.T) {
 	ts := httptest.NewServer(ankiHandler)
 	defer ts.Close()
 
-	t.Run("/anki doesn't accept GET", func(t *testing.T) {
+	t.Run("anki doesn't accept GET", func(t *testing.T) {
 		resp, err := http.Get(ts.URL)
 		if err != nil {
 			t.Error(err)
 		}
+		defer resp.Body.Close()
 		if resp.StatusCode != http.StatusMethodNotAllowed {
 			t.Errorf("got %d; want %d", resp.StatusCode, http.StatusMethodNotAllowed)
 		}
 	})
 
-	t.Run("/anki only accepts application/json", func(t *testing.T) {
+	t.Run("anki only accepts application/json", func(t *testing.T) {
 		resp, err := http.Post(ts.URL, "text/plain", nil)
 		if err != nil {
 			t.Error(err)
 		}
+		defer resp.Body.Close()
 		if resp.StatusCode != http.StatusUnsupportedMediaType {
 			t.Errorf("got %d; want %d", resp.StatusCode, http.StatusUnsupportedMediaType)
 		}
 	})
 
-	t.Run("/anki expects a valid JSON Body", func(t *testing.T) {
+	t.Run("anki expects a valid JSON Body", func(t *testing.T) {
 		body := strings.NewReader("{This is an invalid JSON body")
 		resp, err := http.Post(ts.URL, "application/json", body)
 		if err != nil {
 			t.Error(err)
 		}
-		if resp.StatusCode != http.StatusBadRequest {
-			t.Errorf("got %d; want %d", resp.StatusCode, http.StatusBadRequest)
-		}
-	})
-
-	t.Run("/anki", func(t *testing.T) {
-		body := strings.NewReader(`{"test": "hi"}`)
-		resp, err := http.Post(ts.URL, "application/json", body)
-		if err != nil {
-			t.Error(err)
-		}
+		defer resp.Body.Close()
 		if resp.StatusCode != http.StatusBadRequest {
 			t.Errorf("got %d; want %d", resp.StatusCode, http.StatusBadRequest)
 		}
