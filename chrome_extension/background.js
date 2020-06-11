0e45ce5ba6c257dec0fc7be6984a4593e07f2ee9
Yeongho Kim <yeonghoey@gmail.com>
2020-06-11 22:57:56 +0900

    Add contextmenu to scrap arbitrary text

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -15,6 +15,34 @@ async function requestAnki(typ, target) {
   return json.basename;
 }
 
+function copyToClipboard(text) {
+  const ta = document.createElement('textarea');
+  ta.style.cssText = 'opacity:0; position:fixed; width:1px; height:1px; top:0; left:0;';
+  ta.value = text;
+  document.body.appendChild(ta);
+  ta.focus();
+  ta.select();
+  document.execCommand('copy');
+  ta.remove();
+}
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: 'euisu-scrap',
+    title: 'Euisu: Scrap',
+    contexts: ['selection'],
+  });
+});
+
+chrome.contextMenus.onClicked.addListener((info) => {
+  if (info.menuItemId === 'euisu-scrap') {
+    const text = info.selectionText.trim();
+    requestAnki('tts', text).then((basename) => {
+      copyToClipboard(`${text}\n[sound:${basename}]`);
+    });
+  }
+});
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.type === 'createTab') {
     const index = sender.tab.index + 1;
