e71d61ff08f63c344f007c1a496f71a2aaf52677
Yeongho Kim <yeonghoey@gmail.com>
2020-06-16 08:30:01 +0900

    Use inline-source-map

--- chrome_extension/webpack.config.js
+++ chrome_extension/webpack.config.js
@@ -2,8 +2,8 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  mode: 'production',
-  devtool: 'source-map',
+  mode: 'development',
+  devtool: 'inline-source-map',
   entry: {
     background: './src/background.js',
     en_dict_naver_com: './src/en_dict_naver_com/en_dict_naver_com.js',
