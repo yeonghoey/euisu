34197c3c5044905cc212ac60cdff6de6d4a8796b
Yeongho Kim <yeonghoey@gmail.com>
2020-06-16 11:36:40 +0900

    Migrating to ts

--- chrome_extension/.eslintrc.js
+++ chrome_extension/.eslintrc.js
@@ -1,11 +1,18 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
+  env: {
+      browser: true,
+      webextensions: true,
+  },
   plugins: [
     '@typescript-eslint',
+    'prettier',
   ],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
   ],
 };
