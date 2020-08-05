593695b149047038c71f6b3b0ebf537fea18e62b
Yeongho Kim <yeonghoey@gmail.com>
2020-08-06 07:20:05 +0900

    Update eslint to work on both js and ts

--- chrome_extension/.eslintrc.js
+++ chrome_extension/.eslintrc.js
@@ -1,19 +1,32 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: "module",
+  },
   env: {
     browser: true,
     es2017: true,
     webextensions: true,
   },
   plugins: [
-    '@typescript-eslint',
     'prettier',
   ],
   extends: [
     'eslint:recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [{
+    files: ["**/*.ts"],
+    parser: '@typescript-eslint/parser',
+    plugins: [
+      '@typescript-eslint',
+      'prettier',
+    ],
+    extends: [
+    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
     'prettier/@typescript-eslint',
-  ],
+    ],    
+  }],
 };
