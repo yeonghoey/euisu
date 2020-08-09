4236c443baa5641df003af10fca85345da8ecbf6
Yeongho Kim <yeonghoey@gmail.com>
2020-08-09 15:15:52 +0900

    Print out the started time on echo

--- backend/server/echo.go
+++ backend/server/echo.go
@@ -1,12 +1,16 @@
 package server
 
 import (
+	"fmt"
 	"io"
 	"net/http"
+	"time"
 )
 
 func newEchoHandler() http.Handler {
+	startedAt := time.Now()
+	message := fmt.Sprintf("Euisu started at %v", startedAt)
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "Euisu is working")
+		io.WriteString(w, message)
 	})
 }
