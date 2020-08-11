b9220557c1b23326b329577c3e1c90bffe11aa92
Yeongho Kim <yeonghoey@gmail.com>
2020-08-11 09:11:27 +0900

    Reorder buttons again

--- chrome_extension/src/content/www_youtube_com/pages/watch.ts
+++ chrome_extension/src/content/www_youtube_com/pages/watch.ts
@@ -88,30 +88,29 @@ function createEuisu(
   div.classList.add("euisu");
 
   // Buttons
-
-  const titleButton = makeTitleButton(title);
-  div.appendChild(titleButton);
-
-  const urlButton = makeURLButton(videoId);
-  div.appendChild(urlButton);
+  const screenshotButton = makeScreenshotButton(video);
+  div.appendChild(screenshotButton);
 
   const urlAtCurrentButton = makeURLAtCurrentButton(videoId, video);
   div.appendChild(urlAtCurrentButton);
 
   div.appendChild(makeSpacer());
 
-  const screenshotButton = makeScreenshotButton(video);
-  div.appendChild(screenshotButton);
+  const urlButton = makeURLButton(videoId);
+  div.appendChild(urlButton);
+
+  const titleButton = makeTitleButton(title);
+  div.appendChild(titleButton);
 
   const thumbnailButton = makeThumbnailButton(videoId);
   div.appendChild(thumbnailButton);
 
   // Shortcuts
   const shortcuts: Shortcuts = {
-    Digit1: titleButton,
-    Digit2: urlButton,
-    Digit3: urlAtCurrentButton,
-    Digit4: screenshotButton,
+    Digit1: screenshotButton,
+    Digit2: urlAtCurrentButton,
+    Digit3: urlButton,
+    Digit4: titleButton,
     Digit5: thumbnailButton,
   };
 
